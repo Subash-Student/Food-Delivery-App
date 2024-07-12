7220b5663113d9f14dff43332bd5ba51b9bcd0a2
Subash <subashmurugan2021@gmail.com>
2024-07-12 15:58:28 +0530

    fetch image from database

--- front-end/src/Components/FoodItem/FoodItem.jsx
+++ front-end/src/Components/FoodItem/FoodItem.jsx
@@ -7,12 +7,12 @@ import { StoreContext } from '../../Context/StoreContext';
 const FoodItem = ({id,name,price,description,image}) => {
 
 
-const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
 
   return (
     <div className='food-item'>
       <div className='food-item-img-container'>
-           <img src={image} className='food-item-image' />
+           <img src={`${url}/images/${image}`} className='food-item-image' />
            {!cartItems[id] ? <img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} />
              :<div className='food-item-counter'>
                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
--- front-end/src/Context/StoreContext.jsx
+++ front-end/src/Context/StoreContext.jsx
@@ -1,13 +1,16 @@
 import { createContext,  useEffect,  useState } from "react";
-import {food_list} from "../assets/assets"
+import axios from "axios"
+// import {food_list} from "../assets/assets"
 
 export const StoreContext = createContext(null);
+
 
 const StoreContextProvider = (props) =>{
 
     const[cartItems,setCartItems] = useState({});
     const url = "http://localhost:4000";
     const [token,setToken] = useState("");
+    const[food_list,setFoodList] = useState([]);
 
     const addToCart = (itemId)=>{
         
@@ -34,10 +37,24 @@ const getTotalCartAmount = ()=>{
     }
     return totalAmount;
 }
+
+const fetchFoodList = async()=>{
+    const response = await axios.get(url+'/api/food/list');
+    setFoodList(response.data.data);
+
+
+}
+
+
+
 useEffect(()=>{
-    if(localStorage.getItem("token")){
-        setToken(localStorage.getItem("token"))
+    async function loadData(){
+        await fetchFoodList();
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"))
+        }
     }
+    loadData();
 },[])
     const contextValue = {
     food_list,
